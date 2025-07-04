434e67b4d59285cee936409ac6f01938cbd7bfca
mehedy201 <mehedy643@gmail.com>
2025-07-05 00:32:52 +0600

    The tasks that have been completed for the tickets landing page are: 1. The tickets section is complete and responsive. 2. The user will be able to select tickets and change the quantity of tickets. 3. The price will be shown based on the tickets price and quantity. The order summary will be shown live somewhere and if you continue, you will go to the next step or page.

--- src/pages/TicketSaleLandingPage/TicketsComponts/TicketsComponents.jsx
+++ src/pages/TicketSaleLandingPage/TicketsComponts/TicketsComponents.jsx
@@ -19,20 +19,20 @@ const TicketsComponents = () => {
     return (
         <div>
             {/* Low and Middle Income Countries____________________________________________ */}
-            <div className='sm:flex justify-between pb-5 border-b-1 border-gray-300'>
+            <div className='sm:flex gap-2 justify-between pb-5 border-b-1 border-gray-300'>
                 <div>
-                    <h2 className='text-[22px] font-bold pb-1'>Low and Middle Income Countries</h2>
-                    <h5 className='font-semibold pb-2'>Attendees from Low and Middle-Income Countries As designated by the World Bank / Participants des pays à revenu faible et intermédiaire, tel que désigné par la Banque mondiale</h5>
+                    <h2 className='text-[18px] md:text-[22px] font-bold pb-1'>Low and Middle Income Countries</h2>
+                    <h5 className='text-[14px] md:text-[16px] font-semibold pb-2'>Attendees from Low and Middle-Income Countries As designated by the World Bank / Participants des pays à revenu faible et intermédiaire, tel que désigné par la Banque mondiale</h5>
                     <p className='text-gray-500'>Sales end on <span className='font-bold text-black'>Oct 22, 2025</span></p>
                 </div>
                 <div>
-                    <h2 className='font-bold pb-1'>US $400.00</h2>
+                    <h2 className='font-bold pb-1'>US $440.00</h2>
                     <div className="flex items-center justify-between border border-gray-300 rounded-md">
                         <span onClick={() => {
                             if(lowTicketsQuantity > 0){
                                 setLowTicketsQuantity(lowTicketsQuantity - 1)
-                                setLowTicketsPrice(lowTicketsPrice - 400)
-                                setTotalParice(totalPrice - 400)
+                                setLowTicketsPrice(lowTicketsPrice - 440)
+                                setTotalParice(totalPrice - 440)
                             }else{
                                 return
                             }
@@ -42,8 +42,8 @@ const TicketsComponents = () => {
                         <input className="text-center" style={{width: '40px', display: 'flex'}} value={lowTicketsQuantity} type="text" readOnly/>
                         <span onClick={() => {
                             setLowTicketsQuantity(lowTicketsQuantity + 1)
-                            setLowTicketsPrice(lowTicketsPrice + 400)
-                            setTotalParice(totalPrice + 400)
+                            setLowTicketsPrice(lowTicketsPrice + 440)
+                            setTotalParice(totalPrice + 440)
                         }} className="px-3 py-2 cursor-pointer">
                             <FiPlus/>
                         </span>
@@ -52,10 +52,10 @@ const TicketsComponents = () => {
                 </div>
             </div>
             {/* Full Conference Registration____________________________________________ */}
-            <div className='sm:flex justify-between py-5 border-b-1 border-gray-300'>
+            <div className='sm:flex gap-2 justify-between py-5 border-b-1 border-gray-300'>
                 <div>
-                    <h2 className='text-[22px] font-bold pb-1'>Full Conference Registration</h2>
-                    <h5 className='font-semibold pb-2'>Full Conference Registration/Inscription Complète </h5>
+                    <h2 className='text-[18px] md:text-[22px] font-bold pb-1 '>Full Conference Registration</h2>
+                    <h5 className='text-[14px] md:text-[16px] font-semibold pb-2'>Full Conference Registration/Inscription Complète </h5>
                     <p className='text-gray-500'>Sales end on <span className='font-bold text-black'>Oct 22, 2025</span></p>
                 </div>
                 <div>
@@ -85,10 +85,10 @@ const TicketsComponents = () => {
                 </div>
             </div>
             {/* Corporate Attendees____________________________________________ */}
-            <div className='sm:flex justify-between py-5 border-b-1 border-gray-300'>
+            <div className='sm:flex gap-2 justify-between py-5 border-b-1 border-gray-300'>
                 <div>
-                    <h2 className='text-[22px] font-bold pb-1'>Corporate Attendees</h2>
-                    <h5 className='font-semibold pb-2'>Representatives from Corporations, Organizations, and Businesses /Représentants d'entreprises, d'organisations et de sociétés</h5>
+                    <h2 className='text-[18px] md:text-[22px] font-bold pb-1'>Corporate Attendees</h2>
+                    <h5 className='text-[14px] md:text-[16px] font-semibold pb-2'>Representatives from Corporations, Organizations, and Businesses /Représentants d'entreprises, d'organisations et de sociétés</h5>
                     <p className='text-gray-500'>Sales end on <span className='font-bold text-black'>Oct 22, 2025</span></p>
                 </div>
                 <div>
